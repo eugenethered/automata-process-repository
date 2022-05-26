0007b5ef1113d011f77bd8600ab782253d54656c
Eugene the Red <me@eugenethered.com>
2022-05-26 10:58:38 +0100

    updating deps + none

--- processrepo/repository/serialize/process_deserializer.py
+++ processrepo/repository/serialize/process_deserializer.py
@@ -5,9 +5,10 @@ from processrepo.ProcessRunProfile import RunProfile
 
 
 def deserialize_process(process) -> Process:
-    market = as_data(process, 'market')
-    name = as_data(process, 'name')
-    instant = as_data(process, 'instant')
-    run_profile = RunProfile.parse(as_data(process, 'run_profile'))
-    status = ProcessStatus.parse(as_data(process, 'status'))
-    return Process(market, name, instant, run_profile, status)
+    if process is not None:
+        market = as_data(process, 'market')
+        name = as_data(process, 'name')
+        instant = as_data(process, 'instant')
+        run_profile = RunProfile.parse(as_data(process, 'run_profile'))
+        status = ProcessStatus.parse(as_data(process, 'status'))
+        return Process(market, name, instant, run_profile, status)
